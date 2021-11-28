3fe5ccaba1a521e728b4393dc1b0b1766d623bc4
Lera Lyakisheva <leralyakisheva@gmail.com>
2021-11-28 14:16:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 80;
+        int bonus = 20;
+        int miles = cost / bonus;
+        System.out.println(miles);
+
+
+    }
+}
